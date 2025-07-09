9ee6a12a27acd79cc6f83b57bda5899b892e9887
Stan <75632684+Stan-tab@users.noreply.github.com>
2025-07-09 00:07:19 -0700

    feat: some changes

--- src/rootes/Card.jsx
+++ src/rootes/Card.jsx
@@ -1,7 +1,8 @@
 import { useLoaderData, useNavigate, useOutletContext } from "react-router-dom";
 import style from "./Card.module.css";
 import arrowImg from "../assets/arrow-left.svg";
-import addShopCart from "../assets/addShopCart.svg"
+import addShopCart from "../assets/addShopCart.svg";
+import { useRef, useState } from "react";
 
 export function loader(req) {
   return req.params.id;
@@ -11,6 +12,8 @@ export default function Card() {
   const id = useLoaderData();
   const { data } = useOutletContext();
   const navidate = useNavigate();
+  const ref = useRef(null);
+  const [backgroundColor, setColor] = useState("#6B6B6B");
   let prod = null;
 
   for (const i in data) {
@@ -31,7 +34,19 @@ export default function Card() {
       </button>
       <div className={style.card}>
         <img src={prod.image} alt={prod.title} className={style.mainImg} />
-        <button>
+        <button
+          style={{
+            backgroundColor: prod.inBucket
+              ? "green"
+              : "#6B6B6B" || backgroundColor,
+          }}
+          onClick={() => {
+            prod.inBucket = !prod.inBucket;
+            setColor((prev) => {
+              return prev === "#6B6B6B" ? "green" : "#6B6B6B";
+            });
+          }}
+        >
           <img src={addShopCart} alt="" />
         </button>
         <p>{prod.title}</p>
--- src/rootes/Cart.jsx
+++ src/rootes/Cart.jsx
@@ -4,33 +4,44 @@ import arrowImg from "../assets/arrow-left.svg";
 
 export default function Cart() {
   let { data } = useOutletContext();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   data = data.filter((e) => e.inBucket);
 
   return (
-    <div className={style.box} style={{position:"relative"}}>
-      <button onClick={() => navigate(-1)} style={{position:"absolute", left: 0}}>
+    <div className={style.box} style={{ position: "relative" }}>
+      <button
+        onClick={() => navigate(-1)}
+        style={{ position: "absolute", left: 0, zIndex: 1 }}
+      >
         <img src={arrowImg} alt="back" />
       </button>
-      {data.map((element) => {
-        return (
-          <div key={element.id} className={style.container}>
-            <div className={style.imgContainer}>
-              <img
-                src={element.image}
-                alt={element.title}
-                className={style.img}
-              />
+      <div className={style.prodContainer}>
+        {data.map((element) => {
+          return (
+            <div key={element.id} className={style.container}>
+              <div className={style.imgContainer}>
+                <img
+                  src={element.image}
+                  alt={element.title}
+                  className={style.img}
+                />
+              </div>
+              <p>{element.title}</p>
+              <div className={style.addInfo}>
+                <p>{element.category}</p>
+                <p>{element.price}</p>
+              </div>
+              <p>{element.description}</p>
             </div>
-            <p>{element.title}</p>
-            <div className={style.addInfo}>
-              <p>{element.category}</p>
-              <p>{element.price}</p>
-            </div>
-            <p>{element.description}</p>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <div className={style.price}>
+        <h3>
+          <b>Total</b>
+        </h3>
+        <p>{data.reduce((total, current) => total + current.price, 0)}</p>
+      </div>
     </div>
   );
 }
